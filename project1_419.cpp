03e55415b0b37fada53121264c82f5473cd2bf97
unknown <phucpersie20cr7@gmail.com>
2021-04-05 16:47:33 +0200

    add source

---
+++ project1_419.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "project1_419.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, project1_419, "project1_419" );
---
+++ project1_419GameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "project1_419GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT1_419_API Aproject1_419GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
